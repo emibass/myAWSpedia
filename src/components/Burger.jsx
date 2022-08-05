867306a08bf13da40458585073594a0783600c62
Emilia Fuks <emilia.lippa@gmail.com>
2022-08-06 10:12:52 +1200

    mobile menu

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,30 +1,30 @@
 import React,{useState} from "react";
 import Navbar from "./Navbar";
-import CloseIcon from '@mui/icons-material/Close';
+// import CloseIcon from '@mui/icons-material/Close';
+import {Link} from "react-router-dom";
 
 function Burger () {
 
 const [open, setOpen] = useState(false);
 
+
+
 function Open(){
     setOpen(prev => !prev);
 }
 
-
     return (
         <div>
-        <div className="BurgerIcon" onClick={Open}>
-        {/* <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div> */}
-        {open ? ( <CloseIcon />) : <i className="fa fa-bars"></i> }
         
-        </div>
-      
-        <div>
+        <div className="BurgerIcon" onClick={Open}> 
+         <i className="fa fa-bars"></i>
+   
+        </div>  
+        <div className="homeIcon"><Link to= "/"> <i className="fa fa-home"></i></Link></div>
         {open ? ( <Navbar /> ) : null}
-        </div>
-        </div>
+      
+      
+        </div> 
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 
 function Navbar() {
+
+const [clicked, setClosed] = useState(false);
+
+function Close(){
+    setClosed(true);
+}
     return (
-    <div className="topnav">
-       <div className ="navItem"><Link to= "/">Home</Link></div>
-       <div className ="navItem"><Link to = "/Analytics">Analytics</Link></div> 
-       <div className ="navItem"><Link to = "/Compute">Compute</Link></div> 
-       <div className ="navItem"><Link to = "/Database">Database</Link></div> 
-       <div className ="navItem"><Link to = "/Developer-Tools">Developer-Tools</Link></div> 
-       <div className ="navItem"><Link to = "/Machine-Learning">Machine-Learning</Link></div> 
-       <div className ="navItem"><Link to = "/Management-Governance">Management-Governance</Link></div> 
-       <div className ="navItem"><Link to = "/Networking-Content-Delivery">Networking-Content-Delivery</Link></div> 
-       <div className ="navItem"><Link to = "/Security-Identity-Compliance">Security-Identity-Compliance</Link></div> 
-       <div className ="navItem"><Link to = "/Cloud-Financial-Management">Cloud-Financial-Management</Link></div>
+        
+        <div>
+        { clicked ? null :
+   <div className="topnav"> 
+       <div className ="navItem" onClick={Close}><Link to= "/">Home</Link></div>
+       <div className ="navItem" onClick={Close}><Link to = "/Analytics">Analytics</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Compute">Compute</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Database">Database</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Developer-Tools">Developer-Tools</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Machine-Learning">Machine-Learning</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Management-Governance">Management-Governance</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Networking-Content-Delivery">Networking-Content-Delivery</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Security-Identity-Compliance">Security-Identity-Compliance</Link></div> 
+       <div className ="navItem" onClick={Close}><Link to = "/Cloud-Financial-Management">Cloud-Financial-Management</Link></div>
+       </div> }
        </div>
     )
 };
